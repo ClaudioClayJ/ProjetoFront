ac8fc42ffce18111df77080a12a143287d51ec00
Claudio Clay <claudio_junior05@hotmail.com>
2024-06-04 12:56:53 -0300

    Revert "telas adicionais criadas"
    This reverts commit 5ab8fadcffe41da396cef5a3276f057632fbf14a.

--- src/pages/componentes/menu/index.js
+++ src/pages/componentes/menu/index.js
@@ -1,23 +1,11 @@
 import {Link} from 'react-router-dom'
-import {FiTag,FiTruck,FiShoppingCart,FiGrid, FiUser, FiHome} from "react-icons/fi";
 
 export default function Menu(){
     return(
     <div>
         <nav>
             <h1>Menu</h1>
-             <Link to="/listadashboard" className='link'><FiHome className='icons-menu'size={18}/> Home</Link>
-
-             <Link to="/listausuario" className='link'><FiUser className='icons-menu'size={18}/> Usuário </Link>
-               
-             <Link to="/listaproduto" className='link'><FiTag className='icons-menu'size={18}/> Produto </Link> 
-
-             <Link to="/listaentrada" className='link'><FiShoppingCart className='icons-menu'size={18}/> Entrada </Link>   
-
-             <Link to="/listaestoque" className='link'><FiGrid className='icons-menu'size={18}/> Estoque </Link>
-
-             <Link to="/listasaida" className='link'><FiTruck className='icons-menu'size={18}/> Saída </Link>   
-               
+             <Link to="" className='Link'> Usuário </Link>   
         </nav>
     </div>
     )
--- src/router.js
+++ src/router.js
@@ -3,7 +3,6 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom'
 
 import Logon from './pages/logon'
 import Dashboard from './pages/dashboard'
-import Cadastrousuario from './pages/cadastroUsuario'
 
 
 export default function Rotas(){
@@ -12,7 +11,6 @@ export default function Rotas(){
         <Routes>
         <Route path="/" exact element={<Logon />} />
         <Route path="/dashboard"  element={<Dashboard />} />
-        <Route path="/cadastroUsuario"  element={<Cadastrousuario />} />
 
 
         </Routes>
